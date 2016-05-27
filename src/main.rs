23e19751f44963276d0760ba249980f69904e78b
Rusty Fausak <rustyfausak@gmail.com>
2016-05-27 00:32:00 -0500

    adding docopt
    holy cow --version works

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+extern crate docopt;
+extern crate rustc_serialize;
+
+use docopt::Docopt;
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+const USAGE: &'static str = "
+Rocket League replay parser.
+
+Usage:
+  rlparse [options] <file>
+  rlparse (--help | --version)
+
+Options:
+  -h, --help     Show this help.
+  -V, --version  Show version.
+  -v, --verbose  Verbose output.
+";
+
+#[derive(Debug, RustcDecodable)]
+struct Args {
+    arg_file: Vec<String>,
+    flag_verbose: bool,
+    flag_help: bool,
+    flag_version: bool
+}
+
+fn main() {
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| Ok(d.version(Some(VERSION.to_string()))))
+        .and_then(|d| d.decode())
+        .unwrap_or_else(|e| e.exit());
+
+    println!("{:?}", args);
+}
